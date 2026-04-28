819243ad4cf8e11f0b06dfe387021535cae32d01
cameronduff <cameronsduff@gmail.com>
2026-04-28 16:47:03 +0100

    Refactor main function to use logger for output and add new execution module

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
+from loguru import logger
+
+from uuid import uuid4
+
+APP_NAME = "ai_hedge_fund"
+USER_ID = str(uuid4())
+SESSION_ID = str(uuid4())
+
+
 def main():
-    print("Hello from ai-hedge-fund!")
+    logger.info(f"App name: {APP_NAME}")
+    logger.info(f"User ID: {USER_ID}")
+    logger.info(f"Session ID: {SESSION_ID}")
 
 
 if __name__ == "__main__":
