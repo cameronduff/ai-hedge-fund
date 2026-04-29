e3af67fdb3339f5df2c430441390479d9e0d9588
cameronduff <cameronsduff@gmail.com>
2026-04-29 11:29:38 +0100

    Updating overall hierarchy

--- app/agents/investors/chief_investment_officer/agent.py
+++ app/agents/investors/chief_investment_officer/agent.py
@@ -5,8 +5,41 @@ from app.agents.investors.chief_investment_officer.prompt import (
     CHIEF_INVESTMENT_OFFICER_PROMPT,
 )
 
+from app.agents.investors.aswath_damodaran_agent.agent import aswath_damodaran_agent
+from app.agents.investors.ben_graham_agent.agent import ben_graham_agent
+from app.agents.investors.bill_ackman_agent.agent import bill_ackman_agent
+from app.agents.investors.cathie_wood_agent.agent import cathie_wood_agent
+from app.agents.investors.charlie_munger_agent.agent import charlie_munger_agent
+from app.agents.investors.michael_burry_agent.agent import michael_burry_agent
+from app.agents.investors.monish_pabrai_agent.agent import monish_pabrai_agent
+from app.agents.investors.nassim_taleb_agent.agent import nassim_taleb_agent
+from app.agents.investors.peter_lynch_agent.agent import peter_lynch_agent
+from app.agents.investors.phil_fisher_agent.agent import phil_fisher_agent
+from app.agents.investors.rakesh_jhunjhunwala_agent.agent import (
+    rakesh_jhunjhunwala_agent,
+)
+from app.agents.investors.stanley_druckenmiller_agent.agent import (
+    stanley_druckenmiller_agent,
+)
+from app.agents.investors.warren_buffet_agent.agent import warren_buffet_agent
+
 chief_investment_officer_agent = LlmAgent(
     "chief_investment_officer_agent",
     model=settings.REASONING_MODEL,
     instruction=CHIEF_INVESTMENT_OFFICER_PROMPT,
+    sub_agents=[
+        aswath_damodaran_agent,
+        ben_graham_agent,
+        bill_ackman_agent,
+        cathie_wood_agent,
+        charlie_munger_agent,
+        michael_burry_agent,
+        monish_pabrai_agent,
+        nassim_taleb_agent,
+        peter_lynch_agent,
+        phil_fisher_agent,
+        rakesh_jhunjhunwala_agent,
+        stanley_druckenmiller_agent,
+        warren_buffet_agent,
+    ],
 )
--- app/agents/main_agent.py
+++ app/agents/main_agent.py
@@ -4,8 +4,8 @@ from app.agents.quants.quants_orchestrator_agent.agent import quants_orchestrato
 from app.agents.investors.investors_orchestrator_agent.agent import (
     investors_orchestrator_agent,
 )
-from app.agents.management.executive_orchestrator_agent.agent import (
-    executive_orchestrator_agent,
+from app.agents.management.portfolio_manager_agent.agent import (
+    portfolio_manager_agent,
 )
 
 chief_of_staff_agent = SequentialAgent(
@@ -13,6 +13,6 @@ chief_of_staff_agent = SequentialAgent(
     sub_agents=[
         quants_orchestrator_agent,
         investors_orchestrator_agent,
-        executive_orchestrator_agent,
+        portfolio_manager_agent,
     ],
 )
