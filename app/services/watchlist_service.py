c52670faaa5f7d7439e4b7bf2c46a7bda278354d
cameronduff <cameronsduff@gmail.com>
2026-05-12 20:20:53 +0100

    adding 50 day moving average

--- app/services/watchlist_service.py
+++ app/services/watchlist_service.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from app.clients.trading212_client import Trading212Client
+from app.clients.yfinance_client import YFinanceClient
 
 
 class WatchlistService():
@@ -28,6 +29,18 @@ class WatchlistService():
                 current_stock_interest.add(ticker)
         
         return current_stock_interest
+    
+    def get_50_day_moving_average_devation_percentage(yfinance_ticker: str):
+        yfinance_client = YFinanceClient()
+        historical_data = yfinance_client.get_historical_data(ticker="AAPL", period="50d")
+        ma50 = historical_data['Close'].mean()
+        current_price = yfinance_client.get_current_price(ticker="AAPL")
+
+        return round((current_price - ma50) / ma50 * 100, 2)
+
 
 if __name__ == "__main__":
-    pass
+    yfinance_client = YFinanceClient()
+    current_price = yfinance_client.get_current_price(ticker="AAPL")
+
+    print(current_price)
